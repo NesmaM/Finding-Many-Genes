f6c7e1db158370c6abeebf2b19da08585d04a502
Nesma Abouzaid <59185742+NesmaM@users.noreply.github.com>
2023-09-26 12:13:03 -0700

    Update Part3.java

--- Part3.java
+++ Part3.java
@@ -1,7 +1,7 @@
 
 /**
  * @author (Nesma Abouzaid) 
- * @version (6/20/2023)
+ * @version (6/26/2023)
  */
 public class Part3 {
         public int findStopCodon(String dna, int startIndex, String stopCodon) {
